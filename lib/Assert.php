92f9d90d939195f8645abeea96c342ae0948fe47
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-09-05 22:16:28 +0200

    :sparkles: feat(lib): create assert class

---
+++ lib/Assert.php
@@ -0,0 +1,19 @@
+<?php namespace SIKessEm\Tester;
+
+class Assert {
+
+  public static function that(mixed $value, mixed ...$values): SingleValue|SeveralValues {
+
+    return empty($values)? self::singleValue($value): self::severalValues($value, ...$values); 
+  }
+
+  public static function singleValue(mixed $value): SingleValue {
+
+    return new SingleValue($value);
+  }
+
+  public static function severalValues(mixed ... $values): SeveralValues {
+
+    return new SeveralValues(...$values);
+  }
+}
