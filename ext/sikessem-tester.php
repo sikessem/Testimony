8c88bef63aae39ed20f01f4d6eadc3373f636635
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-09-05 22:17:46 +0200

    :sparkles: chore(ext): add assert extension

--- ext/sikessem-tester.php
+++ ext/sikessem-tester.php
@@ -1,1 +1,26 @@
 <?php namespace SIKessEm\Tester;
+
+if(!function_exists('\SIKessEm\Tester\assert_that')) {
+
+  function assert_that(mixed $value, mixed ...$values): SingleValue|SeveralValues {
+
+    return Assert::that($value, ...$values);
+  }
+}
+
+if(!function_exists(('\SIKessEm\Tester\put_error'))) {
+
+  function put_error(string $message, ...$args) {
+
+    fputs(STDERR, (empty($args)? $message: sprintf($message, ...$args)) . PHP_EOL);
+  }
+}
+
+if(!function_exists(('\SIKessEm\Tester\send_error'))) {
+
+  function send_error(string $message, ...$args) {
+
+    put_error($message, ...$args);
+    exit(1);
+  }
+}
